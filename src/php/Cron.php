5cdb24481646f06d6e2da90574dad117a83597fc
Hayden Andreyka <haydenandreyka@gmail.com>
2017-10-06 19:14:57 -0700

    write cron log to output

--- src/php/Cron.php
+++ src/php/Cron.php
@@ -19,5 +19,6 @@
 	// i added a letter in front of "log" because "log" is reserved.. idk why i chose 's'
 	function slog($message) {
 		file_put_contents("../cron_log", date("Y-M-d.H:m") . " >> " . $message . "\n", FILE_APPEND);
+		echo $message;
 	}
 ?>
