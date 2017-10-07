3eb6214116d71ffb7ad0c803be75946858cb4da5
Hayden Andreyka <haydenandreyka@gmail.com>
2017-10-06 19:03:11 -0700

    move cron log to main dir

--- src/php/Cron.php
+++ src/php/Cron.php
@@ -18,6 +18,6 @@
 	
 	// i added a letter in front of "log" because "log" is reserved.. idk why i chose 's'
 	function slog($message) {
-		file_put_contents("cron_log", date("Y-M-d.H:m") . " >> " . $message . "\n", FILE_APPEND);
+		file_put_contents("../cron_log", date("Y-M-d.H:m") . " >> " . $message . "\n", FILE_APPEND);
 	}
 ?>
