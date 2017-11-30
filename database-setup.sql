ccb94dddea73794d19b5c980d9852edad935355b
Hayden Andreyka <haydenandreyka@gmail.com>
2017-11-29 20:08:21 -0700

    update some mysql comments

--- database-setup.sql
+++ database-setup.sql
@@ -1,22 +1,23 @@
 CREATE TABLE `users` (
-	`userid` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'internal reference only',
-	`email` VARCHAR(100) NOT NULL,
-	`password` VARCHAR(255) NOT NULL COMMENT '60 character hash using bcrypt' COLLATE 'utf8_bin',
-	`registered` BIGINT(20) UNSIGNED NOT NULL COMMENT 'date the user signed up',
+	`userid` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The internal User ID',
+	`email` VARCHAR(100) NOT NULL COMMENT 'User email',
+	`password` VARCHAR(255) NOT NULL COMMENT 'Password hash' COLLATE 'utf8_bin',
+	`registered` BIGINT(20) UNSIGNED NOT NULL COMMENT 'User registration date',
 	PRIMARY KEY (`userid`),
 	UNIQUE INDEX `email` (`email`)
 )
 COMMENT='Stores all user data'
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
+AUTO_INCREMENT=6
 ;
 
 CREATE TABLE `accounthistory` (
-	`userid` BIGINT(20) UNSIGNED NOT NULL COMMENT 'user the action is for',
-	`action` INT(10) UNSIGNED NOT NULL COMMENT 'what the action was, see docs for codes',
-	`time` BIGINT(20) UNSIGNED NOT NULL COMMENT 'time since epoch in secs',
-	`ip` VARCHAR(45) NOT NULL COMMENT 'supports ipv4 mapped ip46\'s',
-	`location` TEXT NULL COMMENT 'generated based on ip',
+	`userid` BIGINT(20) UNSIGNED NOT NULL COMMENT 'User the action is for',
+	`action` INT(10) UNSIGNED NOT NULL COMMENT 'Action identifier',
+	`time` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Timestamp',
+	`ip` VARCHAR(45) NOT NULL COMMENT 'IP that generated the action',
+	`location` TEXT NULL COMMENT 'Geolocation of IP',
 	INDEX `id` (`userid`),
 	INDEX `time` (`time`),
 	CONSTRAINT `ACCOUNTHISTORY_FK_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON UPDATE CASCADE ON DELETE CASCADE
@@ -27,14 +28,14 @@ ENGINE=InnoDB
 ;
 
 CREATE TABLE `logintokens` (
-	`userid` BIGINT(20) UNSIGNED NOT NULL COMMENT 'user id',
-	`token` VARCHAR(60) NOT NULL COMMENT 'random bytes' COLLATE 'utf8_bin',
-	`expiry` BIGINT(20) UNSIGNED NOT NULL COMMENT 'may be deleted at any time past expiry.',
-	`hash` VARCHAR(10) NOT NULL COMMENT 'a hash of the token information, is shown to user',
-	`timegenerated` BIGINT(20) UNSIGNED NOT NULL COMMENT 'when the token was generated',
-	`ip` VARCHAR(45) NULL DEFAULT NULL COMMENT 'ip that generated the token',
-	`location` TEXT NULL COMMENT 'determined from ip',
-	`browser` TEXT NULL COMMENT 'browser info if available',
+	`userid` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Internal user id',
+	`token` VARCHAR(60) NOT NULL COMMENT 'Randomly generated token' COLLATE 'utf8_bin',
+	`expiry` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Expiration date of token',
+	`hash` VARCHAR(10) NOT NULL COMMENT 'A short hash for user reference',
+	`timegenerated` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Time created',
+	`ip` VARCHAR(45) NULL DEFAULT NULL COMMENT 'IP that generated the token',
+	`location` TEXT NULL COMMENT 'Geolocation of IP',
+	`browser` TEXT NULL COMMENT 'Browser information string',
 	INDEX `expiry` (`expiry`),
 	INDEX `id` (`userid`),
 	INDEX `hash` (`hash`),
