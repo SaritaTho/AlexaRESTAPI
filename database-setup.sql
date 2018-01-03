7efeb5cba14f4a4960ac6a23d0867a05ff81fe99
Hayden Andreyka <haydenandreyka@gmail.com>
2018-01-02 21:00:51 -0700

    dump SQL instead of manually exporting it

--- database-setup.sql
+++ database-setup.sql
@@ -1,135 +1,133 @@
-CREATE TABLE `accounthistory` (
-	`userid` INT(10) UNSIGNED NOT NULL COMMENT 'User the action is for',
-	`action` INT(10) UNSIGNED NOT NULL COMMENT 'Action identifier',
-	`time` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Timestamp',
-	`ip` VARCHAR(45) NOT NULL COMMENT 'IP that generated the action',
-	`location` TEXT NULL COMMENT 'Geolocation of IP',
-	INDEX `id` (`userid`),
-	INDEX `time` (`time`),
-	CONSTRAINT `FK_ACCOUNTHISTORY_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON UPDATE CASCADE ON DELETE CASCADE
-)
-COMMENT='Stores account security history for users'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.29-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.5.0.5196
+-- --------------------------------------------------------
 
-CREATE TABLE `logintokens` (
-	`userid` INT(10) UNSIGNED NOT NULL COMMENT 'Internal user id',
-	`token` VARCHAR(60) NOT NULL COMMENT 'Randomly generated token' COLLATE 'utf8_bin',
-	`expiry` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Expiration date of token',
-	`hash` VARCHAR(10) NOT NULL COMMENT 'A short hash for user reference',
-	`timegenerated` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Time created',
-	`ip` VARCHAR(45) NULL DEFAULT NULL COMMENT 'IP that generated the token',
-	`location` TEXT NULL COMMENT 'Geolocation of IP',
-	`browser` TEXT NULL COMMENT 'Browser information string',
-	INDEX `expiry` (`expiry`),
-	INDEX `id` (`userid`),
-	INDEX `hash` (`hash`),
-	CONSTRAINT `FK_LOGINTOKENS_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON UPDATE CASCADE ON DELETE CASCADE
-)
-COMMENT='Stores login tokens to keep user sessions.'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `oauth_accesstokens` (
-	`client` INT(10) UNSIGNED NOT NULL COMMENT 'ID of the client',
-	`expiry` BIGINT(20) UNSIGNED NULL DEFAULT NULL COMMENT 'Expiry of the access token, in seconds since epoch',
-	`scope` TEXT NOT NULL COMMENT 'The scope of the token',
-	`token` VARCHAR(256) NOT NULL COMMENT 'Access token string',
-	`userid` INT(10) UNSIGNED NOT NULL COMMENT 'Userid of the user the token is for',
-	`authcode` INT(11) UNSIGNED NULL DEFAULT NULL COMMENT 'The authorization code that generated the token',
-	INDEX `FK_OAUTH_ACCESSTOKENS_USERID` (`userid`),
-	INDEX `FK_OAUTH_ACCESSTOKENS_AUTHCODE` (`authcode`),
-	INDEX `client` (`client`),
-	CONSTRAINT `FK_OAUTH_ACCESSTOKENS_AUTHCODE` FOREIGN KEY (`authcode`) REFERENCES `oauth_authcodes` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT `FK_OAUTH_ACCESSTOKENS_CLIENT` FOREIGN KEY (`client`) REFERENCES `oauth_clients` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT `FK_OAUTH_ACCESSTOKENS_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON UPDATE CASCADE ON DELETE CASCADE
-)
-COMMENT='Stores OAuth access tokens'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
+-- Dumping structure for table alexa.accounthistory
+CREATE TABLE IF NOT EXISTS `accounthistory` (
+  `userid` int(10) unsigned NOT NULL COMMENT 'User the action is for',
+  `action` int(10) unsigned NOT NULL COMMENT 'Action identifier',
+  `time` bigint(20) unsigned NOT NULL COMMENT 'Timestamp',
+  `ip` varchar(45) NOT NULL COMMENT 'IP that generated the action',
+  `location` text COMMENT 'Geolocation of IP',
+  KEY `id` (`userid`),
+  KEY `time` (`time`),
+  CONSTRAINT `FK_ACCOUNTHISTORY_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores account security history for users';
 
-CREATE TABLE `oauth_authcodes` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The internal ID of the authorization code',
-	`client` INT(10) UNSIGNED NOT NULL COMMENT 'Internal ID of the client',
-	`userid` INT(10) UNSIGNED NOT NULL COMMENT 'ID of the user',
-	`scope` TEXT NOT NULL COMMENT 'The scope of the authorization code',
-	`expiry` BIGINT(20) UNSIGNED NOT NULL COMMENT 'Expiry of the authorization grant. Should be a small amount of time.',
-	`redirect_uri` TEXT NULL COMMENT 'The redirect URI of the original authorization request.',
-	`code` TEXT NOT NULL COMMENT 'The authorization code given to the client' COLLATE 'utf8_bin',
-	`used` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Whether the authorization code has been used. 1 for true, and 0 for false.',
-	PRIMARY KEY (`id`),
-	INDEX `FK_OAUTH_AUTHCODES_USERID` (`userid`),
-	INDEX `FK_OAUTH_AUTHCODES_CLIENT` (`client`),
-	CONSTRAINT `FK_OAUTH_AUTHCODES_CLIENTID` FOREIGN KEY (`client`) REFERENCES `oauth_clients` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT `FK_OAUTH_AUTHCODES_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON UPDATE CASCADE ON DELETE CASCADE
-)
-COMMENT='Stores OAuth authorization codes'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=0
-;
+-- Data exporting was unselected.
+-- Dumping structure for table alexa.logintokens
+CREATE TABLE IF NOT EXISTS `logintokens` (
+  `userid` int(10) unsigned NOT NULL COMMENT 'Internal user id',
+  `token` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'Randomly generated token',
+  `expiry` bigint(20) unsigned NOT NULL COMMENT 'Expiration date of token',
+  `hash` varchar(10) NOT NULL COMMENT 'A short hash for user reference',
+  `timegenerated` bigint(20) unsigned NOT NULL COMMENT 'Time created',
+  `ip` varchar(45) DEFAULT NULL COMMENT 'IP that generated the token',
+  `location` text COMMENT 'Geolocation of IP',
+  `browser` text COMMENT 'Browser information string',
+  KEY `expiry` (`expiry`),
+  KEY `id` (`userid`),
+  KEY `hash` (`hash`),
+  CONSTRAINT `FK_LOGINTOKENS_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores login tokens to keep user sessions.';
 
-CREATE TABLE `oauth_clients` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Internal ID of client',
-	`client_id` TEXT NOT NULL COMMENT '(OAuth) A random, permanent client ID. Not to be confused with the id column.',
-	`client_secret` TEXT NOT NULL COMMENT 'A random, non-permanent client secret' COLLATE 'utf8_bin',
-	`url` TEXT NOT NULL COMMENT 'URL to the client\'s homepage',
-	`name` VARCHAR(25) NOT NULL COMMENT 'Friendly client name',
-	`publisher` VARCHAR(25) NULL DEFAULT NULL COMMENT 'Client publisher',
-	`icon` TEXT NULL COMMENT 'Client icon, stored in base64',
-	`user` INT(10) UNSIGNED NOT NULL COMMENT 'The user that created the client',
-	PRIMARY KEY (`id`),
-	INDEX `FK_OAUTH_CLIENTS_USER` (`user`),
-	CONSTRAINT `FK_OAUTH_CLIENTS_USER` FOREIGN KEY (`user`) REFERENCES `users` (`userid`) ON UPDATE CASCADE ON DELETE CASCADE
-)
-COMMENT='Stores information about OAuth clients\r\n'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=0
-;
+-- Data exporting was unselected.
+-- Dumping structure for table alexa.oauth_accesstokens
+CREATE TABLE IF NOT EXISTS `oauth_accesstokens` (
+  `client` int(10) unsigned NOT NULL COMMENT 'ID of the client',
+  `expiry` bigint(20) unsigned DEFAULT NULL COMMENT 'Expiry of the access token, in seconds since epoch',
+  `scope` text NOT NULL COMMENT 'The scope of the token',
+  `token` varchar(256) NOT NULL COMMENT 'Access token string',
+  `userid` int(10) unsigned NOT NULL COMMENT 'Userid of the user the token is for',
+  `authcode` int(11) unsigned DEFAULT NULL COMMENT 'The authorization code that generated the token',
+  KEY `FK_OAUTH_ACCESSTOKENS_USERID` (`userid`),
+  KEY `FK_OAUTH_ACCESSTOKENS_AUTHCODE` (`authcode`),
+  KEY `client` (`client`),
+  CONSTRAINT `FK_OAUTH_ACCESSTOKENS_AUTHCODE` FOREIGN KEY (`authcode`) REFERENCES `oauth_authcodes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_OAUTH_ACCESSTOKENS_CLIENT` FOREIGN KEY (`client`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_OAUTH_ACCESSTOKENS_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores OAuth access tokens';
 
-CREATE TABLE `oauth_redirecturis` (
-	`client` INT(10) UNSIGNED NOT NULL COMMENT 'The client the the redirect URI belongs to',
-	`uri` TEXT NOT NULL COMMENT 'The redirect URI',
-	INDEX `FK_OAUTH_REDIRECTURIS_CLIENT` (`client`),
-	CONSTRAINT `FK_OAUTH_REDIRECTURIS_CLIENT` FOREIGN KEY (`client`) REFERENCES `oauth_clients` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
-)
-COMMENT='Stores redirect URIs for OAuth clients'
-COLLATE='latin1_swedish_ci'
-ENGINE=InnoDB
-;
+-- Data exporting was unselected.
+-- Dumping structure for table alexa.oauth_authcodes
+CREATE TABLE IF NOT EXISTS `oauth_authcodes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The internal ID of the authorization code',
+  `client` int(10) unsigned NOT NULL COMMENT 'Internal ID of the client',
+  `userid` int(10) unsigned NOT NULL COMMENT 'ID of the user',
+  `scope` text NOT NULL COMMENT 'The scope of the authorization code',
+  `expiry` bigint(20) unsigned NOT NULL COMMENT 'Expiry of the authorization grant. Should be a small amount of time.',
+  `redirect_uri` text COMMENT 'The redirect URI of the original authorization request.',
+  `code` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'The authorization code given to the client',
+  `used` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Whether the authorization code has been used. 1 for true, and 0 for false.',
+  PRIMARY KEY (`id`),
+  KEY `FK_OAUTH_AUTHCODES_USERID` (`userid`),
+  KEY `FK_OAUTH_AUTHCODES_CLIENT` (`client`),
+  CONSTRAINT `FK_OAUTH_AUTHCODES_CLIENTID` FOREIGN KEY (`client`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_OAUTH_AUTHCODES_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores OAuth authorization codes';
 
-CREATE TABLE `oauth_refreshtokens` (
-	`id` INT(10) UNSIGNED NOT NULL COMMENT 'ID of the client',
-	`token` VARCHAR(256) NOT NULL COMMENT 'Refresh token string' COLLATE 'utf8_bin',
-	`userid` INT(10) UNSIGNED NOT NULL COMMENT 'Userid of the user the token is for',
-	`authcode` INT(10) UNSIGNED NULL DEFAULT NULL COMMENT 'Authorization code that created the token',
-	`scope` INT(10) UNSIGNED NULL DEFAULT NULL COMMENT 'The scope of the refresh token. Access tokens of more restrictive scopes may be requested.',
-	PRIMARY KEY (`id`),
-	INDEX `FK_OAUTH_REFRESHTOKENS_USERID` (`userid`),
-	INDEX `FK_OAUTH_REFRESHTOKENS_AUTHCODE` (`authcode`),
-	CONSTRAINT `FK_OAUTH_REFRESHTOKENS_AUTHCODE` FOREIGN KEY (`authcode`) REFERENCES `oauth_authcodes` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT `FK_OAUTH_REFRESHTOKENS_ID` FOREIGN KEY (`id`) REFERENCES `oauth_clients` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT `FK_OAUTH_REFRESHTOKENS_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON UPDATE CASCADE ON DELETE CASCADE
-)
-COMMENT='Stores OAuth access tokens'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
+-- Data exporting was unselected.
+-- Dumping structure for table alexa.oauth_clients
+CREATE TABLE IF NOT EXISTS `oauth_clients` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Internal ID of client',
+  `client_id` text NOT NULL COMMENT '(OAuth) A random, permanent client ID. Not to be confused with the id column.',
+  `client_secret` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'A random, non-permanent client secret',
+  `url` text NOT NULL COMMENT 'URL to the client''s homepage',
+  `name` varchar(25) NOT NULL COMMENT 'Friendly client name',
+  `publisher` varchar(25) DEFAULT NULL COMMENT 'Client publisher',
+  `icon` text COMMENT 'Client icon, stored in base64',
+  `user` int(10) unsigned NOT NULL COMMENT 'The user that created the client',
+  PRIMARY KEY (`id`),
+  KEY `FK_OAUTH_CLIENTS_USER` (`user`),
+  CONSTRAINT `FK_OAUTH_CLIENTS_USER` FOREIGN KEY (`user`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores information about OAuth clients\r\n';
 
-CREATE TABLE `users` (
-	`userid` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The internal User ID',
-	`email` VARCHAR(100) NOT NULL COMMENT 'User email',
-	`password` VARCHAR(255) NOT NULL COMMENT 'Password hash' COLLATE 'utf8_bin',
-	`registered` BIGINT(20) UNSIGNED NOT NULL COMMENT 'User registration date',
-	PRIMARY KEY (`userid`),
-	UNIQUE INDEX `UNQUE_EMAIL` (`email`)
-)
-COMMENT='Stores all user data'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=0
-;
+-- Data exporting was unselected.
+-- Dumping structure for table alexa.oauth_redirecturis
+CREATE TABLE IF NOT EXISTS `oauth_redirecturis` (
+  `client` int(10) unsigned NOT NULL COMMENT 'The client the the redirect URI belongs to',
+  `uri` text NOT NULL COMMENT 'The redirect URI',
+  KEY `FK_OAUTH_REDIRECTURIS_CLIENT` (`client`),
+  CONSTRAINT `FK_OAUTH_REDIRECTURIS_CLIENT` FOREIGN KEY (`client`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores redirect URIs for OAuth clients';
+
+-- Data exporting was unselected.
+-- Dumping structure for table alexa.oauth_refreshtokens
+CREATE TABLE IF NOT EXISTS `oauth_refreshtokens` (
+  `id` int(10) unsigned NOT NULL COMMENT 'ID of the client',
+  `token` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'Refresh token string',
+  `userid` int(10) unsigned NOT NULL COMMENT 'Userid of the user the token is for',
+  `authcode` int(10) unsigned DEFAULT NULL COMMENT 'Authorization code that created the token',
+  `scope` int(10) unsigned DEFAULT NULL COMMENT 'The scope of the refresh token. Access tokens of more restrictive scopes may be requested.',
+  PRIMARY KEY (`id`),
+  KEY `FK_OAUTH_REFRESHTOKENS_USERID` (`userid`),
+  KEY `FK_OAUTH_REFRESHTOKENS_AUTHCODE` (`authcode`),
+  CONSTRAINT `FK_OAUTH_REFRESHTOKENS_AUTHCODE` FOREIGN KEY (`authcode`) REFERENCES `oauth_authcodes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_OAUTH_REFRESHTOKENS_ID` FOREIGN KEY (`id`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_OAUTH_REFRESHTOKENS_USERID` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores OAuth access tokens';
+
+-- Data exporting was unselected.
+-- Dumping structure for table alexa.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `userid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The internal User ID',
+  `email` varchar(100) NOT NULL COMMENT 'User email',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'Password hash',
+  `registered` bigint(20) unsigned NOT NULL COMMENT 'User registration date',
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `UNQUE_EMAIL` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores all user data';
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
